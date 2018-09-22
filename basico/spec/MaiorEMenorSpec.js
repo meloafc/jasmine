30813b5849afe32e1e0bea4433b75905a0d5c5d5
Alex Fabiano <afabiano15@gmail.com>
2018-09-21 21:56:09 -0400

    Criando e testando MaiorEMenor

---
+++ basico/spec/MaiorEMenorSpec.js
@@ -0,0 +1,19 @@
+describe("Maior e Menor", function () {
+
+    it("deve entender numeros em ordem nao sequencial", function () {
+        var algoritmo = new MaiorEMenor();
+        algoritmo.encontra([5, 15, 7, 9]);
+
+        expect(algoritmo.pegaMaior()).toEqual(15);
+        expect(algoritmo.pegaMenor()).toEqual(5);
+    });
+
+    it("deve entender numeros em ordem decrescente", function () {
+        var algoritmo = new MaiorEMenor();
+        algoritmo.encontra([9, 8, 7, 6]);
+
+        expect(algoritmo.pegaMaior()).toEqual(9);
+        expect(algoritmo.pegaMenor()).toEqual(6);
+    });
+
+});
